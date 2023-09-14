3a58ac61943925eeaa0e3583c7af1f23cb7b5a69
uenissantos <uenisprogramador@gmail.com>
2023-09-14 12:13:46 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import *  as Styled from './styles/Styled'
+import pro from  './assets/pro.png'
+
+export const App = () => {
+
+
+    const numeroAleatorio = Math.floor(Math.random() *9);
+
+
+
+    function obterDataDoDia() {
+        // Crie um novo objeto Date que representa a data e hora atual
+        const dataAtual = new Date();
+      
+        // Obtenha o dia, mês e ano da data atual
+        const dia = dataAtual.getDate(); // Dia do mês (1 a 31)
+        const mes = dataAtual.getMonth() + 1; // Mês (0 a 11, então adicionamos 1)
+        const ano = dataAtual.getFullYear(); // Ano com quatro dígitos
+      
+        // Formate a data como uma string (por exemplo, "14/09/2023")
+        const dataFormatada = `${dia}/${mes}/${ano}`;
+      
+        return dataFormatada;
+      }
+      const data = obterDataDoDia();
+
+      function redirecionarParaURL() {
+        // Use window.location.href para redirecionar o usuário para a URL especificada
+        window.location.href = 'https://app.betserver.site/MESTREDOSSINAIS';
+      }
+return (
+
+<Styled.Wrrape>
+<div className='wrapper-title'><p className='title'> RESTA {numeroAleatorio} VAGAS DO O APLICATIVO GRATUITO </p>
+</div>
+<div className='image'><img src={pro} alt="" />
+</div>
+
+
+<h1> ACESSO A SALA COM MAIS DE 10 BOTS </h1>
+<h2>DE R$ 97/MêS POR R$0,00 </h2>
+
+
+
+<h3> A PARTIR DE AGORA VOÇÊ TERÁ <span>ACESSO GRATUITO</span> PARA TODOS OS DIAS .COPIE E COLE NOSSAS OPERAÇÕES DE FORMA 100% AUTOMATICA</h3>
+
+
+
+
+<button onClick={redirecionarParaURL}  > <span>vaga gratuita</span> <p>Somente HOJE {data}  </p></button>
+
+
+
+<span>*Esse link pode não funcionar devido aos ataques que estamos recebendo*</span>
+
+<p>Todo sistema no mundo pode ser hackeado, prova disso que os maiores hackers são contratados pelas grandes empresas após invadir o sistema delas, para corrigir e evitar futuros invasores. </p>
+<p>
+
+Nossa inteligência artificial atua nessa falha que foi detectada por nossos programadores, por isso estamos tendo resultados nunca vistos antes.
+</p>
+
+
+
+<p>
+
+✅Resultados do mês anterior:
+</p>
+<img src="https://rendaativa.online/wp-content/uploads/2023/06/download-1.png" alt="resultado anterior" />
+
+
+<p>Desde o ano passado recebo convites de grandes C4SS1NOS para trabalhar na segurança e agora estão convidando minha equipe pra cuidar dessas falhas que dá tanto prejuízo pra eles</p>
+
+<p>Mas a verdade é que dinheiro nenhum pode comprar a liberdade que proporcionamos pra mais de 2567 pessoas ao redor do mundo: 👇</p>
+
+
+<p className='invite'> Agora só resta você fazer parte desse movimento ainda hoje!
+Mas corra antes que (eles) derrubem mais um site nosso. </p>
+
+
+<button  onClick={redirecionarParaURL}  > <span>vaga gratuita</span> <p>Somente HOJE </p></button>
+
+
+
+<div  className='footer'><p>Este site não é afiliado ao Facebook,google ou uol ou a qualquer entidade de anuncios. Fazemos todos os esforços para indicar claramente e mostrar todas as provas do produto e usamos resultados reais. Nós não vendemos o seu e-mail ou qualquer informação para terceiros. Jamais fazemos nenhum tipo de spam. Se você tiver alguma dúvida, sinta-se à vontade para usar o link de contato e falar conosco em horário comercial de Segunda a Sextas das 09h00 ás 18h00. Lemos e respondemos todas as mensagens por ordem de chegada. </p></div>
+
+
+
+
+
+
+</Styled.Wrrape>
+)
+};
---
+++ src/styles/Styled.js
@@ -0,0 +1,117 @@
+import styled, { css } from 'styled-components';
+
+export const Wrrape = styled.div`
+${() => css`
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, b, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+background-color: black;
+display: flex;
+flex-direction: column;
+text-align: center;
+justify-content: center;
+align-items: center;
+p,h1,h2,h3,span{color:white;margin:auto}
+
+span{ font-weight:bold;color:#FF8200}
+
+
+.wrapper-title{ margin:auto; width:100%;}
+.title{font-size:22px}
+
+
+h1{font-size:32px}
+h2{font-size:28px}
+
+h3{font-size:20px; margin:10px 20%}
+
+
+button{animation: large 1s linear infinite;border-radius:8px;background-color:#FF8200;margin:30px; width:25%;span{color:white;font-size:28px;font-weight:bold; letter-spacing:2px};p{font-size:18px;margin:0}}
+
+
+p{font-size:20px;margin:10px 20%}
+
+.invite{color:#FF8200}
+
+
+.footer{p{font-size:12px;margin:10px 10%}}
+
+@keyframes large {
+				from {
+					 width: 250px; height: 80px;
+				}
+
+				to {
+				 width: 280px; height: 82px
+				}
+			}
+
+
+
+
+
+            @media (min-width: 320px) {
+       
+                .title{font-size:18px}
+.image{margin:auto; width:250px;img{width:100%}}
+
+
+h1{font-size:24px}
+h2{font-size:20px}
+
+h3{font-size:18px; margin:10px 10%}
+p{font-size:18px;margin:10px 10%}
+
+
+
+
+
+.footer{background-color:gray;p{font-size:8px}}
+
+      }
+
+
+
+`}
+`;
